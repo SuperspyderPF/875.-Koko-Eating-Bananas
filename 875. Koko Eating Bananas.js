15c7dcb4ea8d3dfd0bbc58a3cef993e3e8adebb6
daniel asherov <djasherov@gmail.com>
2022-09-01 14:59:07 -0800

    first commit

---
+++ 875. Koko Eating Bananas.js
@@ -0,0 +1,21 @@
+var minEatingSpeed = function(piles, h) {
+    
+    let maxPile = Math.max(...piles)
+    let l = 1, r = maxPile
+    let min = maxPile
+
+    while (l <= r) {
+        let k = (l + r) >> 1 
+        let hours = piles.reduce((acc, cur) => {
+            acc += Math.ceil(cur/k) 
+            return acc
+        },0)
+        if (hours <= h) {
+            min = Math.min(min, k)
+            r = k - 1
+        } else if (hours > h) {
+            l = k + 1  
+        } 
+    }
+    return min
+};
